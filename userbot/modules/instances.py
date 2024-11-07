7c88e0b2213bfbc07f3862bf4494d902b942328f
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-11-07 16:16:33 +0700

    Revert "modules: Intances: Try to print out error message"
    This reverts commit 2471cd8330499fa727b4899d5805658a0624bd49.

--- userbot/modules/instances.py
+++ userbot/modules/instances.py
@@ -82,8 +82,8 @@ async def inslogs(insl):
             await insl.edit("`Logs has been sent with: `" + result)
         except FileNotFoundError:
             await insl.edit("`FileNotFoundError: sendLaravelLog.sh`")
-        except Exception as error:
-            await insl.edit("`Fail on exception: `" + error)
+        except Exception:
+            await insl.edit("`Undefined Exception !`")
 
 CMD_HELP.update(
     {
